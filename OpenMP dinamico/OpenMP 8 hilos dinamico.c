7db144b88f3222b08a60983c8062b8f95f8f7c4c
Sergio Escudero Tabares <sergio.escudero@correounivalle.edu.co>
2023-07-20 10:24:10 -0500

    Se agregaron todas las versiones del código

---
+++ OpenMP dinamico/OpenMP 8 hilos dinamico.c
@@ -0,0 +1,42 @@
+
+#include <stdio.h>
+#include <omp.h>
+
+unsigned long long int
+func(int m, int n);
+
+int MAX_THREADS = 8;
+
+void main()
+{
+    int V[] = {10, 11, 12, 13, 10, 11, 12, 13, 10, 11, 12, 13, 10, 11, 12, 13};
+    int lenV = sizeof(V) / sizeof(V[0]);
+
+    unsigned long long int VR[lenV];
+
+#pragma omp parallel num_threads(MAX_THREADS)
+    {
+#pragma omp for schedule(dynamic)
+        for (int i = 0; i < lenV; i++)
+        {
+            VR[i] = func(3, V[i]);
+        }
+    }
+    // Impresion de los resultados
+    printf("V: [");
+    for (int i = 0; i < lenV; i++)
+    {
+        printf("%llu ", VR[i]);
+    }
+    printf("]\n");
+}
+
+unsigned long long int func(int m, int n)
+{
+    if (m == 0)
+        return n + 1;
+    else if (n == 0)
+        return func(m - 1, 1);
+    else
+        return func(m - 1, func(m, n - 1));
+}
